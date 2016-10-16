0cb9ab268e9b25f32c664b14d4830e49ef67bdf7
cho8 <cho8@ualberta.ca>
2016-10-16 17:21:41 -0600

    q4 done?

--- q4_avery.c
+++ q4_avery.c
@@ -10,43 +10,49 @@ int main(int argc, char **argv){
 
   sqlite3 *db; //the database
   sqlite3_stmt *stmt; //the update statement
-  char database_name[25] = "munich.db";
+  //char database_name[25] = "munich.db";
+  char database_name[]= "as2.db";
   //container for assembling our query
   char sql_statement[999];
 
+  if( argc!=5 ){
+    	fprintf(stderr, "Usage: %s <x1> <x2> <y1> <y2>\n", argv[0]);
+    	return(1);
+  }
 
   /*
   Q9 (15 pts)
 
-  Write a C program, in a file called q9.c that takes an airport IATA 
-  code as a parameter and produces the equivalent answer of question 8 
+  Write a C program, in a file called q9.c that takes an airport IATA
+  code as a parameter and produces the equivalent answer of question 8
   above for that airport.
   */
   char sql_statement1[] = "SELECT pt.id "\
-                            "FROM poi_tag pt, poi_index pi "\
+                            "FROM poi_tag pt, poi_rtree pi "\
                                   "WHERE pt.id=pi.id "\
-                                  "AND pt.key = \"class\" "\
-                                  "AND pt.value LIKE \"c%%\" "\
-                                  "AND minLat>= "; 
+                                    "AND pi.minX>= ";
+                                //  "AND pt.key = \"class\" "\
+                                //  "AND pt.value LIKE \"c%%\" "\
 
-  char sql_statement2[] =" AND maxLat<= ";
 
-  char sql_statement3[] =" AND minLon>= ";
+  char sql_statement2[] =" AND pi.maxX<= ";
 
-  char sql_statement4[] =" AND maxLon<= ";
+  char sql_statement3[] =" AND pi.minY>= ";
 
-  
-  //assembling our query                                  
+  char sql_statement4[] =" AND pi.maxY<= ";
+
+
+  //assembling our query
   strcpy(sql_statement, sql_statement1);
   strncat(sql_statement, argv[1], sizeof(argv[1]));
-  strncat(sql_statement, sql_statement2, sizeof(sql_statement2));
+  strncat(sql_statement, sql_statement2, sizeof(sql_statement2)/sizeof(char));
   strncat(sql_statement, argv[2], sizeof(argv[2]));
-  strncat(sql_statement, sql_statement3, sizeof(sql_statement3));
+  strncat(sql_statement, sql_statement3, sizeof(sql_statement3)/sizeof(char));
   strncat(sql_statement, argv[3], sizeof(argv[3]));
-  strncat(sql_statement, sql_statement4, sizeof(sql_statement4));
+  strncat(sql_statement, sql_statement4, sizeof(sql_statement4)/sizeof(char));
   strncat(sql_statement, argv[4], sizeof(argv[4]));
 
-
+  printf("%s",sql_statement);
   int rc;
 
 
@@ -59,11 +65,11 @@ int main(int argc, char **argv){
 
   rc = sqlite3_prepare_v2(db, sql_statement, -1, &stmt, 0);
 
-  if (rc != SQLITE_OK) {  
+  if (rc != SQLITE_OK) {
     fprintf(stderr, "Preparation failed: %s\n", sqlite3_errmsg(db));
     sqlite3_close(db);
     return 1;
-  }    
+  }
   //printing our list
   while((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
     int col;
