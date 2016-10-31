5f851938a0c8524fc412800ef1d9a030c0e42a3a
cho8 <cho8@ualberta.ca>
2016-10-31 01:04:43 -0600

    cleanup dist files

--- dist.h
+++ dist.h
@@ -3,7 +3,6 @@
 
 double mindist_c(Point p, Node n);
 double minmax_c(Point p, Node n);
-void mindist(sqlite3_context* ctx, int nargs, sqlite3_value** values );
-void minmax(sqlite3_context* ctx, int nargs, sqlite3_value** values );
+
 
 #endif
