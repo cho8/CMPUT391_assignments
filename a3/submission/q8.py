26e52fd8c0d79991d3a541805fe1126afbc880f0
TheAspiredOne <altan@ualberta.ca>
2016-11-30 04:23:48 -0700

    added submission file

---
+++ a3/submission/q8.py
@@ -0,0 +1,233 @@
+from collections import Counter
+import sys, re
+import sqlite3
+
+
+curr = dict() #stores the curr subj, pred, obj values
+temp = list() #list that stores the parsed string
+counter = 0
+d_prefix = {} # dictionary that will store prefix mappings
+
+def parsePrefix(dataLine):
+    """
+    checks prefix and then parses it before storing it in d_prefix
+    """
+    dataLine = dataLine.replace(' ', '\t');
+    tag = pref = uri = term = None
+    try:
+        tag, pref, uri, term= dataLine.split('\t');
+    except ValueError:
+        print(">> Invalid prefix format")
+
+    # print(tag, pref, iri, term);
+    if tag =='@prefix' :
+        # check the prefix
+        if pref[-1] != ':':
+            print(">> Missing colon")
+            return False        # Error! Missing Colon
+        if ' ' in uri:
+            print(">> Space in provided prefix URI")
+            return False
+        if uri[0]!='<' and uri[-1]!='>':
+            print(">> Missing opening and closing '<>'")
+            return False
+
+        if uri[-2] != '/' and uri[-2] != '#' and uri[-2] != ":":
+            print(">> Missing backslash/hashtag/colon")
+            return False
+
+
+        # parse the iri
+        if term.strip('\n') == '.':
+            d_prefix[pref] = uri.strip('<>')
+        else:
+            print(">> Missing finalizing period")
+            return False
+        return True
+
+
+
+def parse_rdf(file):
+    line_number = 0
+    flag = 'null'
+
+    with open (file, "r", encoding = 'utf8') as a:
+        with open ("parsed_results.txt", "w", encoding = 'utf8') as b:
+            for lin in a:
+                line_number = line_number+1
+                # global counter
+                # counter=counter+1
+
+                #recording prefix
+                if (("@" in lin) and ('@prefix' in lin)):
+                    if parsePrefix(lin) != True:
+                        print("error, incorrectly formated prefix",line_number)
+                        return False
+                    continue
+
+                #ignoring foreign language tags
+                if "@" in lin and ('@en' not in lin):
+                    temp = lin.replace('\n','').split("\t")
+                    flag = temp[2][-1]
+                    if '@' not in temp[0]:
+                        curr['sub']=temp[0]
+                    if '@' not in temp[1]:
+                        curr['pred']=temp[1]
+                    if '@' not in temp[2]:
+                        curr['obj']=temp[2][:-2]
+                    continue
+
+                    
+
+
+                #getting rid of newline and english tag identifier
+                temp = lin.replace('\n','').replace('@en','').split("\t")
+
+
+                if len(temp) != 3:
+                   print("error because len(temp) is not 3",line_number);
+                   return False
+
+
+                if (temp[0] and temp[1] and temp[2]):
+                    #print('case 1')
+                    flag = temp[2][-1]
+                    curr['sub']=temp[0].strip('\n<>')
+                    curr['pred']=temp[1].strip('\n<>')
+                    curr['obj']=temp[2][:-2].strip('\n<>')
+
+                elif (flag == ';'):
+                    #print('case 2')
+                    flag = temp[2][-1]
+                    curr['pred'] = temp[1].strip('\n<>')
+                    curr['obj'] = temp[2][:-2].strip('\n<>')
+
+                elif (flag == ','):
+                    #print('case 3')
+                    flag = temp[2][-1]
+                    curr['obj'] = temp[2][:-2].strip('\n<>')
+
+                else:
+                    print("no match", temp,line_number,flag)
+                    return False
+
+                if not check(curr['sub'], curr['pred'], curr['obj']):
+                    return False
+                for i in curr:
+                    if ('http' in curr[i]) or ('date' in curr[i]) or ('float' in curr[i]) or ('^' in curr[i]) or ('_:'in curr[i]):
+                        continue
+
+                    #replacing the prefix tag with appropriate actual URIs
+                    re_object = re.search(':',curr[i])
+                    if re_object:
+                        index = re_object.start(0)
+                        #print(index)
+                        curr[i]=curr[i].replace(curr[i][:index+1],d_prefix[curr[i][:index+1]])
+                    if ("'s" in curr[i]):
+                        curr[i] =  curr[i].replace("'s","QUOTEs")
+
+                b.write(curr['sub']+"\t"+curr['pred']+"\t"+curr['obj']+"\n")
+    a.close()
+    b.close()
+    return True
+
+def check(sub, pred, obj):
+
+    #check for valid string
+    if '"' in obj:
+        if (obj.count('"')!=2):
+            print("Wrongly formatted string", obj)
+            return False
+
+    #check for valid date
+    if 'xsd:date' in obj:
+        if ((obj[0] != '"') and (obj[-11] != '"')):
+            print("wrongly formatd date")
+            return False
+        if  ((obj[1:5].isdigit() == False) or (obj[6:8].isdigit() == False) or (obj[9:11].isdigit() == False)):
+            print("wrongly formatted date")
+            return False
+        if ((obj[5] !="-") or (obj[8] != "-")):
+            print("wrongly formatted date")
+            return False
+
+    #check for valid float
+    if 'float' in obj:
+        value = obj[1:-12]
+        for i in value:
+            if i.isdigit() == False and i != '.' and i !='-':
+                print("error validating floats", i, value)
+                return False
+        if obj.count('.') != 1:
+            return False
+        if obj.count('-') > 1:
+            return False
+
+    #check for non Negative integers
+    if 'nonNegativeInteger' in obj:
+        if obj[1:-25].isdigit() == False:
+            print("wrongly formatted nonNegativeInteger")
+            return False
+
+    if 'Geometry' in obj:
+        value = obj[7:-20]
+        for i in value:
+            if ((i != '-') and (i.isdigit()==False) and (i != ' ') and (i != '.')):
+                print("error processing Geometry")
+                return False
+        if '.' in value:
+            if value.count('.')>2:
+                print("error expected only 2 periods")
+                return False
+        if value.count(" ") !=1:
+            print("error expected 1 space")
+            return False
+
+    return True
+
+
+
+
+def write_to_db(sqldb):
+    """
+    Writes to DB
+    """
+    conn = sqlite3.connect(sqldb)
+    c = conn.cursor()
+    c.execute ('CREATE TABLE rdf (sub TEXT, pred TEXT, obj TEXT);')
+
+    data = ''
+
+    with open('parsed_results.txt', 'r',encoding = 'utf8') as rslt:
+        for lin in rslt:
+            lin=lin.strip('\n')
+            result = lin.split('\t')
+            triple = '(\''+result[0]+'\',\''+result[1]+'\',\''+result[2]+'\')'
+            data = data+triple+','
+    sql_statement = "INSERT INTO rdf VALUES" + data[0:-1] + ";"
+    #print(sql_statement)
+    c.execute(sql_statement)
+    conn.commit()
+    conn.close()
+
+
+###### Main ###########################
+if __name__ == "__main__":
+    argv = sys.argv
+    if len(argv) == 3:
+        sqldb = argv[1]
+        filename = argv[2]
+        if (parse_rdf(filename)):
+            print("processing passed, writing to db")
+            write_to_db(sqldb)
+            print("write complete")
+        else:
+            print("error processing the document")
+
+    else:
+        print("Usage: "+ argv[0] + " <sql-database> <rdf>\n")
+
+
+
+
+##### END #############################
---
+++ a3/submission/q9.py
@@ -0,0 +1,335 @@
+import sys, re
+import sqlite3
+
+file_lines = []
+
+d_prefix = {} # dictionary that will store prefix mappings
+select = {} # variables
+pattern_list = [] # collection of patterns to retrieve
+variables = [] # queried variables
+relation = {} # relationship between variables
+end_result = [] # holder of results
+
+def open_file(file):
+    """
+    Opens and stores lines of a file, assuming sparql query is of a manageable size
+    """
+    with open (file, "r", encoding = 'utf8') as a:
+        # with open ("parsed_results.txt", "w", encoding = 'utf8') as b:
+        for lin in a:
+            file_lines.append(lin.strip('\n'))
+    #  print(file_lines)
+    a.close()
+
+def parse_prefix(dataLine):
+    """
+    checks prefix and then parses it before storing it in d_prefix
+    """
+    dataLine = dataLine.replace(' ', '\t');
+    lin = dataLine.split('\t');
+
+    # print(tag, pref, iri, term);
+    while(lin):
+        tag = lin.pop(0)
+        # Find where PREFIX is indicated
+        if tag =='PREFIX' :
+
+            # check the prefix
+            seg = lin.pop(0)
+            if seg[-1]==":" :       # case 1: PREFIX pref: <uri> (space after pref)
+                pref = seg
+                uri = lin.pop(0)
+
+                if uri[0]!='<' and uri[-1]!='>':
+                    print(">> Missing opening and closing '<>'")
+                    return False
+
+                if ' ' in uri:
+                    print(">> Malformed URI")
+                    return False
+
+                if uri[-2] != '/' and uri[-2] != '#' and uri[-2] != ":":
+                    print(">> Missing backslash/hashtag/colon")
+                    return False
+
+            elif re.search("<[\w.:/#]+>", seg) :     # case 2: PREFIX pref:<uri> (no space)
+                m = re.search("<[\w.:/#]+>", seg)
+                pref = seg[:m.start()-1]
+                uri = seg[m.start()+1:m.end()-1]
+
+            else:
+                print(">> Malformed prefix declaration")
+                return False
+
+        # parse the iri
+        if pref and uri:
+            d_prefix[pref] = uri.strip('<>')
+            # print(d_prefix)
+        else:
+            print(">> Malformed prefix declaration")
+            return False
+        return True
+
+def parse_select(dataLine):
+    """
+    Parses and stores the variables used in select
+    """
+    dataLine = dataLine.replace(' ', '\t')
+    statement = dataLine.split('\t')
+
+    if statement[0].upper() == 'SELECT':
+        if statement[1] == '*':
+            print("### TODO: All variables Return");
+        if statement[2] == 'WHERE':
+            parse_where(dataLine)
+            return True
+        else :
+            for s in statement[1:]:
+                s=s.strip('\n')
+                if s.upper == 'WHERE':
+                    return
+                elif s== '':
+                    continue
+                if s=='':
+                    continue;
+                select[s]='';
+                variables.append(s)
+
+                print('SELECT: ',select)
+                print('VARIABLES: ', variables)
+            return True
+
+def parse_where(dataLine):
+    """
+    Parse the where close of the sparql query, including patterns
+    """
+    dataline = dataLine.replace(' ', '\t')
+    statement = dataline.split('\t')
+    if "WHERE" in dataline and statement[-1]=='{':
+        # read the next line after where
+        dataline = file_lines.pop(0).strip('\t ')
+        if dataline == '':
+            dataline=file_lines.pop(0).strip('\t ')
+        statement = dataline.replace(' ','\t').split('\t')
+
+        while (statement[0] != '}'):    # while within WHERE block
+            print(statement)
+            if dataLine:
+                compile_patterns(statement)
+
+            # read the next line
+            dataline = file_lines.pop(0).strip('\t ')
+            if dataline == '':
+                dataline=file_lines.pop(0).strip('\t ')
+            statement = dataline.replace(' ','\t').split('\t')
+
+        return True
+
+    else:
+        print(">> Malformed 'where' declaration")
+        return False
+
+
+def compile_patterns(dataLine):
+    """
+    Compiles patterns and sorts them into processing order
+    """
+    # print(dataLine)
+    i = 0;
+
+    for s in dataLine:
+        m = re.search(':', s)
+
+        # TODO Filter
+
+        # If a prefix is used
+        if m :
+            prefix = s[:m.start(0)+1]
+
+            if not prefix or not prefix in d_prefix.keys():
+                print(">> Undefined prefix '{}'".format(prefix))
+                return false
+
+            s=s.replace(prefix, d_prefix[prefix])
+            dataLine[i]=s
+        i+=1
+
+    # find if more than one variable in pattern_stack, rearrange to bottom of stack
+    num_var = sum(1 for s in dataLine if '?' in s)
+    if num_var == 1:
+        pattern_list.append({'sub': dataLine[0], 'pred':dataLine[1], 'obj':dataLine[2]})
+    else:
+        pattern_list.insert(0,{'sub':dataLine[0], 'pred':dataLine[1], 'obj':dataLine[2]})
+
+
+
+def parse_sparql(file):
+    """
+    Starting point for parsing the sparql query
+    """
+
+    while file_lines:
+        lin = file_lines.pop(0)
+
+        #recording prefix
+        if ("PREFIX" in lin):
+            if parse_prefix(lin) != True:
+                print("error, incorrectly formatted prefix")
+                return False
+            continue
+
+        elif ("SELECT" in lin):
+            if parse_select(lin) != True:
+                print("error, incorrectly formatted select statement")
+                return False
+            continue
+
+        # Parse WHERE patterns
+        elif ("WHERE" in lin):
+            if parse_where(lin) != True:
+                print("error, incorrectly formatted where declaration")
+                return False
+            continue
+
+        # end elif WHERE
+    # EOF
+
+
+
+def build_query():
+    """
+    Build the query to retrieve from sqlite db using the parsed patterns
+    """
+    basequery = 'SELECT * FROM rdf'
+    query= ''
+    while pattern_list:
+        print('############################')
+        pattern = pattern_list.pop()
+        print('PATTERN: ', pattern)
+        var = [i for i in pattern.items() if '?' in i[1]] # vars
+        defd = [i for i in pattern.items() if not '?' in i[1]]  # defined uris
+        print("stuff", var)
+
+        # Only one variable
+        if len(var)==1:
+            var = var[0]    # get rid of list characteristic
+            print("SINGLE VAR --")
+            if var[1] not in select.keys():
+                select[var[1]] = []
+
+            query = basequery.replace('*', '{}'.format(var[0]))
+            # build the query using other triple elements
+            query += " where {}=\'{}\' and {}=\'{}\'".format(defd[0][0], defd[0][1], defd[1][0], defd[1][1])
+
+            print('QUERY: ',query)
+
+            # # associate query with this variable (or intersect it with existing one)
+            if (select[var[1]]):
+                select[var[1]] += ' intersect ' + query
+            else:
+                select[var[1]]= query
+            print(select)
+
+            relation[(var[1],)]=query
+
+
+        # Two var in one pattern
+        elif len(var) == 2:
+            print("DOUBLE VAR --")
+            print(var)
+
+            # query = basequery.replace('*', '{}, {}'.format(var[0][0], var[1][0]))
+            query = basequery
+            # find the triple associated with this relationship
+            # include the criteria for the known uri
+            query += " where {}=\'{}\'".format(defd[0][0], defd[0][1])
+            print(query)
+            # append related variables
+            for v in var:
+                if select[v[1]]:
+                    query += " and {} in ({})".format(v[0], select[v[1]])
+                else:
+                    query += " and {} not in ({})".format(v[0], select[v[1]])
+            relation[(pattern['sub'], pattern['pred'], pattern['obj'])] = query
+            print(relation)
+
+
+
+def check(sub, pred, obj):
+    if obj[0]=='"':
+        if obj[-1]!='"':
+            print("checking failed, unclosed quotations")
+            return False
+
+
+def read_from_db(sqldb):
+    """
+    Writes to DB
+    """
+    conn = sqlite3.connect(sqldb)
+    c = conn.cursor()
+    res = []
+
+    what_to_return=variables
+    if not variables:
+        what_to_return = select.keys();
+
+
+
+    for r in relation.keys():
+        vars_in_relation = list(filter(lambda x: x in select.keys(), r))
+
+        if len(vars_in_relation) >= len(what_to_return):
+            res = c.execute(relation[r])
+            for rows in res:
+                resultrow = []
+                for v in what_to_return:
+                    if (not v in r):
+                        print(">> Variable not defined in query")
+                        return False;
+                    i = r.index(v)
+                    resultrow.append(rows[i])
+
+                end_result.append(resultrow)
+
+            return True
+
+
+
+    conn.commit()
+    conn.close()
+
+def print_result():
+
+    # Print out the headers
+    what_to_return=variables
+    if not variables:
+        what_to_return = select.keys();
+
+    for w in what_to_return:
+        print(w, end='\t\t\t\t')
+    print()
+
+    # Print out results
+    for row in end_result:
+        print('\t\t'.join(row))
+
+###### Main ###########################
+if __name__ == "__main__":
+    argv = sys.argv
+    if len(argv) == 3:
+        sqldb = argv[1]
+        filename = argv[2]
+        open_file(filename)
+        parse_sparql(filename)
+        build_query();
+        read_from_db(sqldb)
+        print_result();
+
+    else:
+        print("Usage: "+ argv[0] + " <sql-database> <sparql-query>\n")
+
+
+
+
+##### END #############################
