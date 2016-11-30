ea9d77f229cf02884b90785c858a37f22d87eac6
TheAspiredOne <altan@ualberta.ca>
2016-11-29 18:07:45 -0700

    added checks

--- a3/q8.py
+++ a3/q8.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import sys, re
 import sqlite3
 
@@ -39,30 +40,45 @@ def parsePrefix(dataLine):
 
 
 def parse_rdf(file):
+    line_number = 0
     flag = 'null'
+
     with open (file, "r", encoding = 'utf8') as a:
         with open ("parsed_results.txt", "w", encoding = 'utf8') as b:
             for lin in a:
+                print(flag,line_number)
+                line_number = line_number+1
                 # global counter
                 # counter=counter+1
 
                 #recording prefix
                 if (("@" in lin) and ('@prefix' in lin)):
                     if parsePrefix(lin) != True:
-                        print("error, incorrectly formated prefix")
+                        print("error, incorrectly formated prefix",line_number)
                         return False
                     continue
 
                 #ignoring foreign language tags
                 if "@" in lin and ('@en' not in lin):
+                    temp = lin.replace('\n','').split("\t")
+                    flag = temp[2][-1]
+                    if '@' not in temp[0]:
+                        curr['sub']=temp[0]
+                    if '@' not in temp[1]:
+                        curr['pred']=temp[1]
+                    if '@' not in temp[2]:
+                        curr['obj']=temp[2][:-2]
                     continue
+
+                    
+
 
                 #getting rid of newline and english tag identifier
                 temp = lin.replace('\n','').replace('@en','').split("\t")
 
 
                 if len(temp) != 3:
-                   print("error because len(temp) is not 3");
+                   print("error because len(temp) is not 3",line_number);
                    return False
 
 
@@ -85,10 +101,11 @@ def parse_rdf(file):
                     curr['obj'] = temp[2][:-2]
 
                 else:
-                    print("no match", temp)
+                    print("no match", temp,line_number,flag)
                     return False
 
-                check(curr['sub'], curr['pred'], curr['obj'])
+                if not check(curr['sub'], curr['pred'], curr['obj']):
+                    return False
                 for i in curr:
                     if ('http' in curr[i]) or ('date' in curr[i]) or ('float' in curr[i]) or ('^' in curr[i]) or ('_:'in curr[i]):
                         continue
@@ -97,24 +114,76 @@ def parse_rdf(file):
                     re_object = re.search(':',curr[i])
                     if re_object:
                         index = re_object.start(0)
-                        print(index)
+                        #print(index)
                         curr[i]=curr[i].replace(curr[i][:index+1],d_prefix[curr[i][:index+1]])
                     if ("'s" in curr[i]):
-                        curr[i] =  curr[i].replace("'s","QUOTE")
+                        curr[i] =  curr[i].replace("'s","QUOTEs")
 
                 b.write(curr['sub']+"\t"+curr['pred']+"\t"+curr['obj']+"\n")
     a.close()
     b.close()
+    return True
 
 def check(sub, pred, obj):
-    if obj[0]=='"':
-        if obj[-1]!='"':
-            print("checking failed, unclosed quotations")
+
+    #check for valid string
+    if '"' in obj:
+        if (obj.count('"')!=2):
+            print("Wrongly formatted string", obj)
             return False
 
+    #check for valid date
+    if 'xsd:date' in obj:
+        if ((obj[0] != '"') and (obj[-11] != '"')):
+            print("wrongly formatd date")
+            return False
+        if  ((obj[1:5].isdigit() == False) or (obj[6:8].isdigit() == False) or (obj[9:11].isdigit() == False)):
+            print("wrongly formatted date")
+            return False
+        if ((obj[5] !="-") or (obj[8] != "-")):
+            print("wrongly formatted date")
+            return False
+
+    #check for valid float
+    if 'float' in obj:
+        value = obj[1:-12]
+        for i in value:
+            if i.isdigit() == False and i != '.' and i !='-':
+                print("error validating floats", i, value)
+                return False
+        if obj.count('.') != 1:
+            return False
+        if obj.count('-') > 1:
+            return False
+
+    #check for non Negative integers
+    if 'nonNegativeInteger' in obj:
+        if obj[1:-25].isdigit() == False:
+            print("wrongly formatted nonNegativeInteger")
+            return False
+
+    if 'Geometry' in obj:
+        value = obj[7:-20]
+        for i in value:
+            if ((i != '-') and (i.isdigit()==False) and (i != ' ') and (i != '.')):
+                print("error processing Geometry")
+                return False
+        if '.' in value:
+            if value.count('.')>2:
+                print("error expected only 2 periods")
+                return False
+        if value.count(" ") !=1:
+            print("error expected 1 space")
+            return False
+
+    return True
 
 
-def two_same(string)
+
+
+
+
+def two_same(string):
    for i in range(len(string)-1):
       if string[i] == string[i+1]:
          return True
@@ -126,17 +195,17 @@ def write_to_db(sqldb):
     """
     conn = sqlite3.connect(sqldb)
     c = conn.cursor()
-    c.execute ('CREATE TABLE rdf (sub TEXT, pred TEXT, obj TEXT)')
+    #c.execute ('CREATE TABLE rdf (sub TEXT, pred TEXT, obj TEXT)')
 
     data = ''
 
-    with open('parsed_results.txt', 'r') as rslt:
+    with open('parsed_results.txt', 'r',encoding = 'utf8') as rslt:
         for lin in rslt:
             result = lin.split('\t')
             triple = '(\''+result[0]+'\',\''+result[1]+'\',\''+result[2]+'\')'
             data = data+triple+','
     sql_statement = "INSERT INTO rdf VALUES" + data[0:-1] + ";"
-    print(sql_statement)
+    #print(sql_statement)
     c.execute(sql_statement)
     conn.commit()
     conn.close()
@@ -148,10 +217,11 @@ if __name__ == "__main__":
     if len(argv) == 3:
         sqldb = argv[1]
         filename = argv[2]
-        parse_rdf(filename)
-        write_to_db(sqldb)
-        #print(sqldb,filename)
-
+        if (parse_rdf(filename)):
+            print("processing passed, writing to db")
+            write_to_db(sqldb)
+        else:
+            print("error processing the document")
     else:
         print("Usage: "+ argv[0] + " <sql-database> <rdf>\n")
 
