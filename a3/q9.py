6c33e9c3bd6f306ccf026e6b6f25f1dd06b20200
cho8 <cho8@ualberta.ca>
2016-11-30 01:40:24 -0700

    working ish q9.py

--- a3/q9.py
+++ a3/q9.py
@@ -1,17 +1,30 @@
 import sys, re
 import sqlite3
 
+file_lines = []
+
 d_prefix = {} # dictionary that will store prefix mappings
 select = {} # variables
 pattern_list = [] # collection of patterns to retrieve
 variables = [] # queried variables
 relation = {} # relationship between variables
 
+def open_file(file):
+    """
+    Opens and stores lines of a file, assuming sparql query is of a manageable size
+    """
+    with open (file, "r", encoding = 'utf8') as a:
+        # with open ("parsed_results.txt", "w", encoding = 'utf8') as b:
+        for lin in a:
+            file_lines.append(lin.strip('\n'))
+    #  print(file_lines)
+    a.close()
+
 def parse_prefix(dataLine):
     """
     checks prefix and then parses it before storing it in d_prefix
     """
-    dataLine = dataLine.replace(' ', '\t').strip('\n');
+    dataLine = dataLine.replace(' ', '\t');
     lin = dataLine.split('\t');
 
     # print(tag, pref, iri, term);
@@ -66,24 +79,56 @@ def parse_select(dataLine):
     if statement[0].upper() == 'SELECT':
         if statement[1] == '*':
             print("### TODO: All variables Return");
+        if statement[2] == 'WHERE':
+            parse_where(dataLine)
             return True
         else :
             for s in statement[1:]:
+                s=s.strip('\n')
                 if s.upper == 'WHERE':
                     return
                 elif s== '':
                     continue
-                select[s.strip('\n')]='';
-                variables.append(s.strip('\n'))
+                if s=='':
+                    continue;
+                select[s]='';
+                variables.append(s)
 
                 print('SELECT: ',select)
                 print('VARIABLES: ', variables)
             return True
 
+def parse_where(dataLine):
+    dataline = dataLine.replace(' ', '\t')
+    statement = dataline.split('\t')
+    if "WHERE" in dataline and statement[-1]=='{':
+        # read the next line after where
+        dataline = file_lines.pop(0).strip('\t ')
+        if dataline == '':
+            dataline=file_lines.pop(0).strip('\t ')
+        statement = dataline.replace(' ','\t').split('\t')
+
+        while (statement[0] != '}'):    # while within WHERE block
+            print(statement)
+            if dataLine:
+                compile_patterns(statement)
+
+            # read the next line
+            dataline = file_lines.pop(0).strip('\t ')
+            if dataline == '':
+                dataline=file_lines.pop(0).strip('\t ')
+            statement = dataline.replace(' ','\t').split('\t')
+
+        return True
+
+    else:
+        print(">> Malformed 'where' declaration")
+        return False
+
+
 def compile_patterns(dataLine):
+    # print(dataLine)
     i = 0;
-    if (dataLine==''):
-        return
 
     for s in dataLine:
         m = re.search(':', s)
@@ -101,7 +146,6 @@ def compile_patterns(dataLine):
             s=s.replace(prefix, d_prefix[prefix])
             dataLine[i]=s
         i+=1
-
     # find if more than one variable in pattern_stack, rearrange stack
     num_var = sum(1 for s in dataLine if '?' in s)
     if num_var == 1:
@@ -112,50 +156,34 @@ def compile_patterns(dataLine):
 
 
 def parse_sparql(file):
-    flag = 'null'
-    with open (file, "r", encoding = 'utf8') as a:
-        # with open ("parsed_results.txt", "w", encoding = 'utf8') as b:
-            for lin in a:
-                # global counter
-                # counter=counter+1
 
-                #recording prefix
-                if ("PREFIX" in lin):
-                    if parse_prefix(lin) != True:
-                        print("error, incorrectly formatted prefix")
-                        return False
-                    continue
+    while file_lines:
+        lin = file_lines.pop(0)
 
-                elif ("SELECT" in lin):
-                    if parse_select(lin) != True:
-                        print("error, incorrectly formatted select statement")
-                        return False
-                    continue
+        #recording prefix
+        if ("PREFIX" in lin):
+            if parse_prefix(lin) != True:
+                print("error, incorrectly formatted prefix")
+                return False
+            continue
 
-                # Parse WHERE patterns
-                elif ("WHERE" in lin):
-                    dataline = lin.replace(' ', '\t').strip('\n')
-                    statement = dataline.split('\t')
-                    if statement[0].upper() == 'WHERE' and statement[1] == '{':
-                        # read the next line after where
-                        dataline = a.readline().strip('\t\n ')
-                        statement = dataline.replace(' ','\t').split('\t')
+        elif ("SELECT" in lin):
+            if parse_select(lin) != True:
+                print("error, incorrectly formatted select statement")
+                return False
+            continue
 
-                        while (statement[0] != '}'):    # while within WHERE block
+        # Parse WHERE patterns
+        elif ("WHERE" in lin):
+            if parse_where(lin) != True:
+                print("error, incorrectly formatted where declaration")
+                return False
+            continue
 
-                            compile_patterns(statement)
+        # end elif WHERE
+    # EOF
 
-                            # read the next line
-                            dataline = a.readline().strip('\t\n ')
-                            statement = dataline.replace(' ','\t').split('\t')
 
-                        # end while
-                    # end elif WHERE
-                # EOF, get the data from DB.
-                # end function
-
-    a.close()
-    # b.close()
 
 def build_query():
     basequery = 'SELECT * FROM rdf'
@@ -172,6 +200,8 @@ def build_query():
         if len(var)==1:
             var = var[0]    # get rid of list characteristic
             print("SINGLE VAR --")
+            if var[1] not in select.keys():
+                select[var[1]] = []
 
             query = basequery.replace('*', '{}'.format(var[0]))
             # build the query using other triple elements
@@ -186,14 +216,16 @@ def build_query():
                 select[var[1]]= query
             print(select)
 
+            relation[(var[1],)]=query
+
 
         # Two var in one pattern
         elif len(var) == 2:
             print("DOUBLE VAR --")
             print(var)
 
-            query = basequery.replace('*', '{}, {}'.format(var[0][0], var[1][0]))
-            # query = basequery
+            # query = basequery.replace('*', '{}, {}'.format(var[0][0], var[1][0]))
+            query = basequery
             # find the triple associated with this relationship
             # include the criteria for the known uri
             query += " where {}=\'{}\'".format(defd[0][0], defd[0][1])
@@ -204,7 +236,7 @@ def build_query():
                     query += " and {} in ({})".format(v[0], select[v[1]])
                 else:
                     query += " and {} not in ({})".format(v[0], select[v[1]])
-            relation[(var[0][1],var[1][1])] = query
+            relation[(pattern['sub'], pattern['pred'], pattern['obj'])] = query
             print(relation)
 
 
@@ -216,34 +248,6 @@ def check(sub, pred, obj):
             return False
 
 
-
-def two_same(string):
-   for i in range(len(string)-1):
-      if string[i] == string[i+1]:
-         return True
-   return False
-
-def write_to_db(sqldb):
-    """
-    Writes to DB
-    """
-    conn = sqlite3.connect(sqldb)
-    c = conn.cursor()
-    c.execute ('CREATE TABLE rdf (sub TEXT, pred TEXT, obj TEXT)')
-
-    data = ''
-
-    with open('parsed_results.txt', 'r') as rslt:
-        for lin in rslt:
-            result = lin.split('\t')
-            triple = '(\''+result[0]+'\',\''+result[1]+'\',\''+result[2]+'\')'
-            data = data+triple+','
-    sql_statement = "INSERT INTO rdf VALUES" + data[0:-1] + ";"
-    # print(sql_statement)
-    c.execute(sql_statement)
-    conn.commit()
-    conn.close()
-
 def read_from_db(sqldb):
     """
     Writes to DB
@@ -252,12 +256,28 @@ def read_from_db(sqldb):
     c = conn.cursor()
     res = []
 
-    for k in relation.keys():
-        if list(k)==variables:
-            res = c.execute(relation[k])
+    what_to_return=variables
+    if not variables:
+        what_to_return = select.keys();
+    print(what_to_return)
 
-    for row in res:
-        print(row)
+
+    for r in relation.keys():
+        vars_in_relation = list(filter(lambda x: x in select.keys(), r))
+
+        if len(vars_in_relation) >= len(what_to_return):
+            res = c.execute(relation[r])
+            for rows in res:
+                for v in what_to_return:
+                    if (not v in r):
+                        print(">> Variable not defined in query")
+                        return False;
+                    i = r.index(v)
+                    print(rows[i], end='  ', flush=True)
+                print() # newline
+            return True
+
+
 
     conn.commit()
     conn.close()
@@ -274,6 +294,7 @@ if __name__ == "__main__":
     if len(argv) == 3:
         sqldb = argv[1]
         filename = argv[2]
+        open_file(filename)
         parse_sparql(filename)
         build_query();
         read_from_db(sqldb)
